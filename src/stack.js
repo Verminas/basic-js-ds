0a9155c9edf578404e540818743a470219b71ce8
Verminas <veremeeva96@gmail.com>
2023-12-24 21:43:50 +0300

    feat: work with remove-from-list task, 1 test doesn't pass

--- src/stack.js
+++ src/stack.js
@@ -13,20 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  constructor(array) {
+    this.array = array;
+    this.lastIndex = this.array.length - 1;
+  }
+
+  push(element) {
+   // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.array[this.lastIndex + 1] = element;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (this.array.length > 0) {
+      this.peek();
+      this.array.splice(this.lastIndex, 1);
+    } else {
+      return undefined;
+    }
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.array[this.lastIndex], 1;
   }
+  // getLength() {
+  //   for (let i = 0; )
+  // }
 }
 
 module.exports = {
